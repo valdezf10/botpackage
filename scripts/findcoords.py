1f2c08a3eb8d144ed1e43641ddfbf8410e5ff55d
Jason Dekarske <dekarskej@gmail.com>
2019-02-28 20:32:04 -0800

    This will track 2d pose estimates from rviz and write the x and y coordinates to a csv file. useful for choosing waypoints.

---
+++ scripts/findcoords.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+import csv
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+def callback(gotpose):
+    gotx = gotpose.pose.pose.position.x
+    goty = gotpose.pose.pose.position.y
+    with open('coords.csv', 'a') as csvFile:
+        writer = csv.writer(csvFile)
+        print('got point')
+        writer.writerow([gotx, goty])
+
+    csvFile.close()
+    
+def listener():
+    rospy.init_node('coord_listener')
+    rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == "__main__":
+    listener()
