5b9c8151d29b9b6a009c7ab7fef250ff05cafc70
Jonathan Dorsey <jonathandorsey1117@gmail.com>
2019-03-07 12:39:53 -0800

    planners are now implemented as environment variables

--- launch/Sim_Director.py
+++ launch/Sim_Director.py
@@ -2,19 +2,21 @@
 
 import rospy
 import os 
+import roslaunch
 from time import sleep
 
 
-plannerList = ['true', 'false'] # true = Dijkstra Planner, false = A*
+#plannerList = ['true', 'false'] # true = Dijkstra Planner, false = A*
+plannerList = ['false', 'true']
 sensorList = [1,2,3] # Number of sensors to change 
 
-os.environ["WAITFORIT"]="true"
+#os.environ["WAITFORIT"]="true"
 print("sofarsogood")
 for counter in range(1,4):
 		if counter == 1:
 			os.environ['LASER_RANGE_VAL']=str(1.35)
 			print("if ONE") 
-		        sleep(5)
+		       
 			print("done sleeping")	
 		elif counter == 2: 
 			os.environ['LASER_RANGE_VAL']=str(3.5)
@@ -24,10 +26,10 @@ for counter in range(1,4):
 			print("if THREE")
 			
 		for planner in plannerList:
-			commandString = "roslaunch botpackage Sim_Launch_Compact.launch use_dijkatra:="
-			commandString += planner
-			os.system(commandString)
-			testval =0
+			os.environ["use_dijkstra"]=planner
+			os.system("roslaunch botpackage Sim_Launch_Compact.launch")
+			#print(process.is_alive())
+			
 			while os.environ["WAITFORIT"] == true:
 				if testval ==0:
 					print("I am Waiting for Action to finish")
