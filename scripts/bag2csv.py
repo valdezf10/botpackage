16afa3c9b125cd48f51c71ba30a2d34c3074d813
Jason Dekarske <dekarskej@gmail.com>
2019-03-09 16:34:07 -0800

    successful data to csv. not sure if values are correct

--- scripts/bag2csv.py
+++ scripts/bag2csv.py
@@ -9,28 +9,50 @@ import numpy as np
 
 
 #convert to coordinates to path length
-def odomdata(currenttopic):
+def odomdata(currenttopic, endtimes):
 	poselist = []
-	for subtopic, msg, t in currenttopic:	
-		x = float(msg.pose.pose.position.x)
-		y = float(msg.pose.pose.position.y)
-		poselist.append([x,y])
-	#filewriter.writerow(values)
-	posearray = np.array(poselist)
-	pathlength= posearray.diff(axis=0).sum(axis=0)
-	finallength = np.sqrt(pathlength[0]**2+pathlength[1]**2)
-	print finallength
-
-def goaldata(currenttopic):
-	poselist = np.array([])
 	for subtopic, msg, t in currenttopic:	
 		x = msg.pose.pose.position.x
 		y = msg.pose.pose.position.y
-		np.append(poselist, [x,y])
-	#filewriter.writerow(values)
-	length = np.diff(poselist)
-	totallength = np.sqrt(length[0]**2+length[1]**2)
-	print totallength
+		currenttime = float(msg.header.stamp.nsecs)/float(10**9) + msg.header.stamp.secs
+		poselist.append([x,y,currenttime])
+	splitlengths = []
+	lastendtime = 0.0
+	for endtime in endtimes:
+		split = []
+		for counter,x in enumerate(poselist):
+			if lastendtime<= poselist[counter][2] <=endtime:
+				split.append(poselist[counter])
+		lastendtime = endtime
+		posearray = np.array(split)
+		pathdiff= np.diff(posearray, axis=0)
+		totalpath = np.sum(pathdiff, axis=0)
+		finallength = np.sqrt(totalpath[0]**2+totalpath[1]**2)
+		splitlengths.append(finallength)
+	return splitlengths
+
+def goaldata(currenttopic):
+	goallist = []
+	xlast = 0.0
+	ylast = 0.0
+	for subtopic, msg, t in currenttopic:	
+		x = msg.goal.target_pose.pose.position.x
+		y = msg.goal.target_pose.pose.position.y
+		finallength = np.sqrt((x-xlast)**2+(y-ylast)**2)
+		goallist.append(finallength) 
+		xlast = x
+		ylast = y
+	goallist = np.array(goallist)
+	return goallist
+
+def resultdata(currenttopic):
+	timelist = [0.0]
+	for subtopic, msg, t in currenttopic:	
+		finishtime = float(msg.header.stamp.nsecs)/float(10**9) + msg.header.stamp.secs
+		timelist.append(finishtime)
+	timearray = np.array(timelist)
+	timediff = np.diff(timearray, axis=0)
+	return timediff, timelist[1:]
 
 listOfBagFiles = [f for f in os.listdir("/home/jason/252_bot/src/botpackage/bag") if f[-4:] == ".bag"]	#get list of only bag files in current dir.
 numberOfFiles = str(len(listOfBagFiles))
@@ -55,17 +77,11 @@ for bagFile in listOfBagFiles:
 	# 	filewriter.writerow(headers)
 	# 	firstIteration = False
 
-	#get list of topics from the bag
-	listOfTopics = []
-	for topic, msg, t in bagContents:
-		if topic not in listOfTopics:
-			listOfTopics.append(topic)
-
-	for topicName in listOfTopics:
-		if topicName == '/odom': # or '/move_base/goal':
-			with open("/home/jason/252_bot/src/botpackage/csv" + topicName +".csv", 'w') as csvfile:
-				filewriter = csv.writer(csvfile, delimiter = ',')
-				odomdata(bag.read_messages(topicName))
-
+	result,endtimes = resultdata(bag.read_messages('/move_base/result'))
+	odom = odomdata(bag.read_messages('/odom'), endtimes)
+	goal = goaldata(bag.read_messages('/move_base/goal'))
+	with open("/home/jason/252_bot/src/botpackage/csv/result.csv", 'a') as csvfile: #change to 'a' for append
+		filewriter = csv.writer(csvfile, delimiter = ',')
+		filewriter.writerows(np.column_stack((np.array(odom),goal,result)))
 	bag.close()
 print "Done reading all " + numberOfFiles + " bag files."
