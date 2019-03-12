1e65ecbc207d2d4a5f4fe1184fbc2dac57567420
Jonathan Dorsey <jonathandorsey1117@gmail.com>
2019-03-11 23:19:01 -0700

    in progess for jason A* test

--- launch/timeoutDirector.py
+++ launch/timeoutDirector.py
@@ -4,14 +4,22 @@ import rospy
 import os 
 import roslaunch
 from time import sleep
-
+from randommap import mainProg
 #Iterative Simulation Parameters
 #plannerList = ['false', 'true'] # Dijkstra: true=on, false=off
 plannerList = ['true','false'] # Dijkstra: true=on, false=off
+doorOpen = [True, False]
 
 laserRanges= [.5, 1.5 ,3.5, 5]
 laserRate = [1, 5 , 20, 40,]
 imuRate = [100, 200, 400, 500 ] 
+
+def setDoor(val):
+	if val== True:
+		mainProg(100)
+	elif val ==False:
+		mainProg(0)
+	
 
 
 def Set_Default_Param():
@@ -21,17 +29,15 @@ def Set_Default_Param():
 	#print os.system("echo $LASER_RANGE_VAL")
 	#print os.system("echo $LASER_UPDATE_RATE")
 	#print os.system("echo $IMU_UPDATE_RATE")
-	os.environ["WAITFORIT"]="true"
+	
 	
 def Set_Env_Var(name, value):
 	os.environ[str(name)]=str(value)
 	sleep(1)
 
 def startSim():
-	#os.system("roslaunch botpackage slamtesting.launch")
-	#os.system("roslaunch botpackage Sim_Launch_Compact.launch")
-        os.system("roslaunch botpackage SimLaunch_timeout.launch")
-        #os.system("roslaunch botpackage launch.launch")
+	os.system("roslaunch botpackage SimLaunch_timeout.launch")
+        
 
 	
 def killSim():
@@ -46,12 +52,13 @@ if __name__=="__main__":
 	if setParam ==True:
 		Set_Default_Param()   # Set Default Parameters 
 		setParam== False      
+	setDoor(True)
 
 	for planner in plannerList:			# Iterate over global planners
 		Set_Env_Var("USE_DIJKSTRA",planner) # Set planner env var
 		os.system("echo Dijkstra Value:")
 		os.system("echo $USE_DIJKSTRA") 	# debug print 
-		#while os.environ["WAITFORIT"]=="true":			
+					
 		
 		try: 
 			startSim()	#begin Simulation
@@ -61,15 +68,6 @@ if __name__=="__main__":
 			print("exception made")
 			killSim()
 			continue
-		
-		
-	
-	
-	
-	
-	
-	
-	
 
 
 
@@ -81,38 +79,3 @@ if __name__=="__main__":
 
 
 
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-#for counter in range(1,4):
-#		if counter == 1:
-#			os.environ['LASER_RANGE_VAL']=str(1.35)
-#			print("if ONE") 
-#
-#		       
-#			
-#		for planner in plannerList:
-#			os.environ["use_dijkstra"]=planner
-#			#print(process.is_alive())
-#			
-#			while os.environ["WAITFORIT"] == true:
-#				if testval ==0:
-#					print("I am Waiting for Action to finish")
-#					testval +=1
-##			print("OUTSIDE OF WHILE")
-#
-#
-#
-#
